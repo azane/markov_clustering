be838b762bf03101b41d2b37d7b3717a4cf75be4
azane <azane@nnu.edu>
2018-04-27 00:08:05 -0500

    funcionizes setup for calls by parent modules.

--- setup.py
+++ setup.py
@@ -4,13 +4,18 @@ from Cython.Build import cythonize
 import numpy
 import os
 
-# Meant to be run from its own directory
-abspath = os.path.abspath(__file__)
-dname = os.path.dirname(abspath)
-os.chdir(dname)
 
-setup(
-    name="markov_clustering",
-    ext_modules=cythonize("markov_clustering/c_mcl.pyx"),
-    include_dirs=[numpy.get_include()]
-)
+def do_setup():
+    # Meant to be run from its own directory
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+    os.chdir(dname)
+
+    setup(
+        name="markov_clustering",
+        ext_modules=cythonize("markov_clustering/c_mcl.pyx"),
+        include_dirs=[numpy.get_include()]
+    )
+
+if __name__ == "__main__":
+    do_setup()
