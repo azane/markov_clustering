4f7b079652c59643d2c5f57c335c1af3e2dee11c
azane <azane@nnu.edu>
2018-04-27 13:40:38 -0500

    warns when not built.

--- markov_clustering/__init__.py
+++ markov_clustering/__init__.py
@@ -1,5 +1,10 @@
-from .mcl import *
-from markov_clustering.c_mcl import prune
+import warnings
+
+try:
+    from .mcl import *
+    from markov_clustering.c_mcl import prune
+except ImportError:
+    warnings.warn("markov_clustering not built.")
 
 try:
     from .drawing import *
